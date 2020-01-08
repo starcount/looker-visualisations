4a8b7b9df93e77b95e5ceff378931188fb980e66
Mariusz Pilarczyk <mariusz.pilarczyk@starcount.com>
2020-01-08 15:22:36 +0000

    v2

--- tree.js
+++ tree.js
@@ -1,59 +1,148 @@
-const createTable = (title, current, percentage, contribution) =>
-  `<table width="200">
+numbro.registerLanguage({
+  languageTag: "en-GB",
+  delimiters: {
+    thousands: ",",
+    decimal: "."
+  },
+  abbreviations: {
+    thousand: "k",
+    million: "m",
+    billion: "b",
+    trillion: "t"
+  },
+  ordinal: number => {
+    let b = number % 10;
+    return ~~((number % 100) / 10) === 1
+      ? "th"
+      : b === 1
+      ? "st"
+      : b === 2
+      ? "nd"
+      : b === 3
+      ? "rd"
+      : "th";
+  },
+  currency: {
+    symbol: "£",
+    position: "prefix",
+    code: "GBP"
+  },
+  currencyFormat: {
+    thousandSeparated: true,
+    totalLength: 4,
+    spaceSeparated: true,
+    average: true
+  },
+  formats: {
+    fourDigits: {
+      totalLength: 4,
+      spaceSeparated: true,
+      average: true
+    },
+    fullWithTwoDecimals: {
+      output: "currency",
+      thousandSeparated: true,
+      spaceSeparated: true,
+      mantissa: 2
+    },
+    fullWithTwoDecimalsNoCurrency: {
+      mantissa: 2,
+      thousandSeparated: true
+    },
+    fullWithNoDecimals: {
+      output: "currency",
+      thousandSeparated: true,
+      spaceSeparated: true,
+      mantissa: 0
+    }
+  }
+});
+
+numbro.setLanguage("en-GB");
+
+const createTable = (title, current, percentage, contribution) => {
+  const positive = contribution > 0;
+  const negative = contribution < 0;
+
+  return `<table width="200" class="${positive ? "result-positive" : ""} ${
+    negative ? "result-negative" : ""
+  }">
     <tr>
       <th colspan="2">${title}</th>
     </tr>
     <tr>
       <td>Current</td>
-      <td class="value">${current}</td>
+      <td class="value">${numbro(current).formatCurrency()}</td>
     </tr>
-    <tr>
+    <tr class="percentage">
       <td>%</td>
       <td class="value">${percentage}</td>
     </tr>
-    <tr>
+    <tr class="contribution">
       <td>Contribution</td>
-      <td class="value">${contribution}</td>
+      <td class="value">${numbro(contribution).formatCurrency()}</td>
     </tr>
   </table>`;
+};
 
 looker.plugins.visualizations.add({
   create: function(element, config) {
     element.innerHTML = `
     <style>
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+
     .Treant { position: relative; overflow: hidden; padding: 0 !important; }
-    .Treant > .node,
-    .Treant > .pseudo { position: absolute; display: block; visibility: hidden; }
+    .Treant .node,
+    .Treant .pseudo { position: absolute; display: block; visibility: hidden; 
+      background-color: #FFFFFF; }
     .Treant.Treant-loaded .node,
     .Treant.Treant-loaded .pseudo { visibility: visible; }
     .Treant > .pseudo { width: 0; height: 0; border: none; padding: 0; }
     .Treant .collapse-switch { width: 3px; height: 3px; display: block; border: 1px solid black; position: absolute; top: 1px; right: 1px; cursor: pointer; }
     .Treant .collapsed .collapse-switch { background-color: #868DEE; }
-    .Treant > .node img {	border: none; float: left; }
-      #OrganiseChart-simple { height: 100%; width: 100%; margin: 5px; margin: 5px auto; border: 3px solid #DDD; border-radius: 3px; }
-      .node { color: #9CB5ED; border: 2px solid #C8C8C8; border-radius: 3px; }
+      #OrganiseChart-simple { height: 100%; width: 100%; margin: 5px; margin: 5px auto; }
+      .node { color: #9CB5ED; border: 1px solid #898C8F; border-radius: 6px; width: 240px; transition: box-shadow 0.2s linear; }
       .node p { font-size: 20px; line-height: 20px; height: 20px; font-weight: bold; padding: 3px; margin: 0; }
+
+      .node:hover {
+        box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+      }
+
+      .result-positive .percentage .value, .result-positive .contribution .value, .result-positive th {
+        color: #2ECC40;
+      }
+      
+      .result-negative .percentage .value, .result-negative .contribution .value, .result-negative th {
+        color: #FF4136;
+      }
 
       th {
         text-transform: uppercase;
+        font-family: "Open Sans";
       }
 
       th, td {
-        padding: 4px;
+        padding: 4px 8px;
       }
 
       table {
-        color: black;
+        color: #636666;
+        width: 100%;
         border-collapse: collapse;
+        border-style: hidden;
+      }
+
+      .td {
+        font-family: Arial;
       }
 
       .value {
-        font-family: monospace;
+        font-family: Helvetica;
         text-align: center;
       }
       
-      table, th, td {
-        border: 1px solid black;
+      th, td {
+        border: 1px solid #898C8F;
       }
     </style>
   `;
@@ -61,12 +150,15 @@ looker.plugins.visualizations.add({
     const chartConfig = {
       chart: {
         container: "#OrganiseChart-simple",
+        subTeeSeparation: 50,
+        siblingSeparation: 50,
+        levelSeparation: 100,
         connectors: {
           type: "step",
           style: {
             "arrow-end": "classic-wide-long",
             "stroke-width": 2,
-            stroke: "#665B57"
+            stroke: "#898C8F"
           }
         }
       },
@@ -77,7 +169,7 @@ looker.plugins.visualizations.add({
             innerHTML: createTable("sparks sales", 181, 90, 20),
             children: [
               {
-                innerHTML: createTable("customers", 181, 90, 20)
+                innerHTML: createTable("customers", 1810000, 90, 20)
               },
               {
                 innerHTML: createTable("spend per customer", 181, 90, 20),
@@ -86,7 +178,7 @@ looker.plugins.visualizations.add({
                     innerHTML: createTable("baskets per customer", 181, 90, 20)
                   },
                   {
-                    innerHTML: createTable("spend per basket", 181, 90, 20),
+                    innerHTML: createTable("spend per basket", -181, 90, 20),
                     children: [
                       {
                         innerHTML: createTable("units per basket", 181, 90, 20)
@@ -107,13 +199,13 @@ looker.plugins.visualizations.add({
                 innerHTML: createTable("baskets", 181, 90, 20)
               },
               {
-                innerHTML: createTable("spend per basket", 181, 90, 20),
+                innerHTML: createTable("spend per basket", 181, 90, -20),
                 children: [
                   {
                     innerHTML: createTable("units per basket", 181, 90, 20)
                   },
                   {
-                    innerHTML: createTable("spend per unit", 181, 90, 20)
+                    innerHTML: createTable("spend per unit", 181, 90, -20)
                   }
                 ]
               }
@@ -128,6 +220,21 @@ looker.plugins.visualizations.add({
 
     setTimeout(() => {
       new Treant(chartConfig);
+
+      const scaleContainer = document.createElement("div");
+
+      const parentWidth = container.getBoundingClientRect().width;
+      const contentWidth = container.childNodes[0].getBoundingClientRect()
+        .width;
+
+      scaleContainer.append(...container.childNodes);
+      container.append(scaleContainer);
+
+      const scale = parentWidth / contentWidth;
+
+      scaleContainer.style.transform = `scale(${scale})`;
+      scaleContainer.style.transformOrigin = "left top";
+      scaleContainer.style.position = "relative";
     }, 1000);
   },
   updateAsync: function(data, element, config, queryResponse, details, done) {
